be85d0ded903b6620bfb48157acf9b3b60323d09
salemokey <flipsh96@gmail.com>
2023-11-13 15:44:40 +0300

    Задача 2 Выполнена

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function task1(str) {
     alert ("Адрес введен правильно!");
     else
     alert ("Адрес не верный!");}
+    
+    function task2(str) {  
+        let re = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
+        let flag = re.test(str);
+        if (flag)
+          alert("Переменная объявлена верно!");
+        else
+          alert("Переменная объявлена неверно!");
+      }
